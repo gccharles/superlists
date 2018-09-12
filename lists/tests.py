3de1ef55e88b3363328b4fd591d2dd688b9405dc
charlesliu <1531240986@qq.com>
2018-09-12 18:09:12 +0800

    5.4

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,8 @@ from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
+import re
+
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -10,10 +12,20 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(re.sub(csrf_regex, '', response.content.decode()), expected_html)
 
-
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
 
 
 
