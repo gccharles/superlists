14493d3d71c12c61e733109fdcbebee475e905d8
charlesliu <1531240986@qq.com>
2018-09-12 18:46:51 +0800

    5.4.3

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+settings.configure()
+
+class Item(models.Model):
+    text = models.TextField(default="")
+
+
